6a562ec48fe4f2b632db80c1bb3ff4091ba49dee
lordayan56-coder <lordayan56@gmail.com>
2026-08-28 11:34:02 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,23 +22,7 @@ android {
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
-
-  signingConfigs {
-    create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
-    } 
-
-  buildTypes {
-    release {
-      isCrunchPngs = false
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
-    }
+  
     debug {
     signingConfig = signingConfigs.getByName("debug")
 }
@@ -55,6 +39,27 @@ android {
     includeInApk = false
     includeInBundle = true
   }
+signingConfigs {
+    create("release") {
+        val keystorePath = System.getenv("KEYSTORE_PATH")
+        storeFile = file(keystorePath)
+        storePassword = System.getenv("STORE_PASSWORD")
+        keyAlias = "upload"
+        keyPassword = System.getenv("KEY_PASSWORD")
+    }
+}
+
+buildTypes {
+    release {
+        isCrunchPngs = false
+        isMinifyEnabled = false
+        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        signingConfig = signingConfigs.getByName("release")
+    }
+
+    debug {
+        signingConfig = signingConfigs.getByName("debug")
+    }
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
